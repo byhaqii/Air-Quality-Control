e56ca7abfd4d9d18d2eb2f02765deddb11274346
Muhammad Khoirul Anwarudin <kanwarudin914@gmail.com>
2026-04-27 01:03:58 +0000

    feat: setup firebase configuration for air quality monitoring

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getAuth } from "firebase/auth"; // 1. Sudah benar importnya
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWw83DGsRTb9PgwLibOofNkC4Tt5MTbtw",
@@ -12,12 +12,9 @@ const firebaseConfig = {
   appId: "1:304115318204:web:78b317c6b7157f8c3b3fb9"
 };
 
-// Inisialisasi agar tidak terjadi error saat hot-reload di Next.js
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
-// 2. Inisialisasi layanan yang ingin digunakan
 const db = getDatabase(app);
-const auth = getAuth(app); // <--- TAMBAHKAN INI
+const auth = getAuth(app);
 
-// 3. Ekspor variabel agar bisa dipakai di file lain
-export { db, auth }; // <--- PASTIKAN 'auth' MASUK DI SINI
+export { db, auth }; 
